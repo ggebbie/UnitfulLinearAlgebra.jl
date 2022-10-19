2675fa305de98d2cbc00961a03b5d1966d6d658c
G Jake Gebbie <ggebbie@whoi.edu>
2022-10-19 14:29:52 -0400

    12805 PSET 2 functions

--- src/UnitfulLinearAlgebra.jl
+++ src/UnitfulLinearAlgebra.jl
@@ -1,5 +1,50 @@
 module UnitfulLinearAlgebra
 
-# Write your package code here.
+export scaling_matrix_unitful, svd_unitful, inv_unitful
+
+"""
+   function scaling_matrix_unitful(γ)
+
+	   Take diagonal dimensional elements γ
+	   Form a weighting or scaling matrix that has the proper units
+	   Note that the off-diagonals are zero but they must have units
+"""
+function scaling_matrix_unitful(γ)
+	γsqrt = .√γ
+	
+	S = γsqrt*γsqrt'
+	N = size(S,1)
+	for i = 1:N
+		for j = 1:N
+			if i != j
+			   # make off-diagonals zero, but keep units
+               S[i,j] -= S[i,j]
+			end
+		end
+	end
+	return S
+end
+
+"""
+	function inv_unitful(E) 
+
+	Take the inverse of a matrix and respect units
+    The second part of the equation string is a way to grab the inverse transpose units
+"""
+inv_unitful(E) = inv(ustrip.(E)) .* unit.( 1 ./ E' )
+
+"""
+	function svd_unitful(E)
+
+		Extend the SVD to be used with unitful quantities.
+		You will unlikely need to do this.
+"""
+function svd_unitful(E)
+	U,L1,V = svd(ustrip.(E),full=true)
+	# in simplest case, put units of E on L
+	uE = unit.(diag(E))
+	L = L1 .* uE
+	return U,L,V
+end
 
 end
